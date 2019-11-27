f81ba25729f43bed0ed664dee7249294e41c9dc8
gsk <37265728+SKgao@users.noreply.github.com>
2019-11-27 21:34:26 +0800

    测试demo

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'GET'
+  | 'get'
+  | 'POST'
+  | 'post'
+  | 'DELETE'
+  | 'delete'
+  | 'HEAD'
+  | 'head'
+  | 'OPTIONS'
+  | 'options'
+  | 'PUT'
+  | 'put'
+  | 'PATCH'
+  | 'patch'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
